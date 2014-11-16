7b48f7279c7ebd84b7c6751aeefcb2f22edb15ee
serco-chen <xg.chen87@gmail.com>
2014-11-16 21:25:35 +0800

    enable definition of route controllers with basic features
    application can define route controllers with dsl like:
    
      - get "/topics" { some controller code }
      - post /\/topics$/ { some controller code }
    
    A lot of issues are not concerned like percent-encoding and security
    problems by far.

--- app.rb
+++ app.rb
@@ -3,4 +3,20 @@ $:.unshift "#{File.dirname(__FILE__)}"
 # app.rb
 require './lib/sinatra'
 
+class Sinatra::Base
+
+  get '/' do
+    ['200', {'Content-Type' => 'text/html'}, ['You are in the root page']]
+  end
+
+  get '/posts' do
+    ['200', {'Content-Type' => 'text/html'}, ['You are in the posts page']]
+  end
+
+  post '/posts' do
+    ['200', {'Content-Type' => 'text/html'}, ['You just post to the posts page']]
+  end
+end
+
+
 Sinatra::Base.run!
--- lib/sinatra/base.rb
+++ lib/sinatra/base.rb
@@ -4,14 +4,24 @@ module Sinatra
   class Base
     def call(env)
       @request = Rack::Request.new(env)
-      if @request.path_info == '/'
-        ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
-      else
-        ['404', {'Content-Type' => 'text/html'}, ['Not found.']]
+
+      # test the path_info with each route controller
+      self.class.routes[@request.request_method].each do |path, block|
+        next unless path.match @request.path_info
+        return block.call
       end
+
+      # return 404 when no routes match
+      ['404', {'Content-Type' => 'text/html'}, ['Not found.']]
     end
 
     class << self
+      attr_reader :routes
+
+      def reset!
+        @routes = {}
+      end
+
       # Instantiate self for each request in case that requests will
       # interfere with each other.
       def call(env)
@@ -22,6 +32,23 @@ module Sinatra
         handler = Rack::Handler.get("puma")
         handler.run(self, {Port: 4567, Host: '0.0.0.0'})
       end
+
+      def get(path, &block)     route 'GET',     path, &block end
+      def head(path, &block)    route 'HEAD',    path, &block end
+      def post(path, &block)    route 'POST',    path, &block end
+      def put(path, &block)     route 'PUT',     path, &block end
+      def patch(path, &block)   route 'PATCH',   path, &block end
+      def delete(path, &block)  route 'DELETE',  path, &block end
+      def options(path, &block) route 'OPTIONS', path, &block end
+
+      # store defined route controllers in @routes, so it can be
+      # retrieved by incoming requests.
+      def route(verb, path, &block)
+        (@routes[verb] ||= []) << [path, block]
+      end
     end
+
+    reset!
+
   end
 end
